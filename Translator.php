53dc4e8fff444d38729a639643e8ab166f6c40d3
Benjamin Lemin <benjamin.lemin@gmail.com>
2016-10-27 16:17:14 +0200

    Update Translator.php

--- Translator.php
+++ Translator.php
@@ -9,6 +9,45 @@
 namespace Mortagus\Library\Intern;
 
 
-class Translator {
+class Translation
+{
+    /**
+     * @var string
+     */
+    private static $language;
+
+    /**
+     * tries to get a translated property by prepending the
+     * configured language to the given entities getter
+     *
+     * @param object $entity
+     * @param string $getter
+     * @return mixed
+     */
+    public static function getTranslated($entity, $getter) {
+        $language = self::getLanguage();
+        $translatedGetter = $getter.$language;
+        if(method_exists($entity, $translatedGetter)) {
+            return $entity->$translatedGetter();
+        } else {
+            return $entity->$getter;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public static function getLanguage()
+    {
+        return self::$language;
+    }
+
+    /**
+     * @param string $language
+     */
+    public static function setLanguage($language)
+    {
+        self::$language = ucfirst(strtolower($language));
+    }
 
 }
